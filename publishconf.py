8c77286020d3c2229e1effa35aaf05280c23aef3
Clemens Lutz <lutzcle@cml.li>
2017-06-22 20:04:02 +0200

    Prepend 'www.' to default public URL

--- publishconf.py
+++ publishconf.py
@@ -10,7 +10,7 @@ import sys
 sys.path.append(os.curdir)
 from pelicanconf import *
 
-SITEURL = 'https://clemenslutz.com'
+SITEURL = 'https://www.clemenslutz.com'
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = None
