13f64951ccac8039a734a706a2bf84df68839e4c
Clemens Lutz <lutzcle@cml.li>
2017-06-22 19:24:54 +0200

    Fix missing icons

--- publishconf.py
+++ publishconf.py
@@ -10,7 +10,7 @@ import sys
 sys.path.append(os.curdir)
 from pelicanconf import *
 
-SITEURL = 'http://www.clemenslutz.com'
+SITEURL = 'http://clemenslutz.com'
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = None
